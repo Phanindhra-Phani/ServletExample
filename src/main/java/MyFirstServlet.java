4f4d8cc354a66d1ebc7bca091617132d9247f182
Sravan Kumar Pala <spala@apple.com>
2016-10-06 08:28:09 +0530

    My Sample Project

---
+++ src/main/java/MyFirstServlet.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyFirstServlet")
+public class MyFirstServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	 public void doGet(HttpServletRequest req, HttpServletResponse response)
+		        throws ServletException, IOException
+    {
+//		 	response.setContentType("application/pdf");
+		 	response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.println("<html><body>");
+			out.println("<h1>This is my first Servlet</h1>");
+			out.println("</body></html>");
+       
+    }
+	 
+	 
+
+}
